5e825c1fe9537891fdc16e66bf95c1ccceb2176a
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-01-04 16:57:47 -0700

    Use divs rather than progress

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -13,43 +13,45 @@ const heights = {
 };
 
 const ProgressBar = ({ value, size }) => {
-  return <Progress value={value} max={100} size={size}></Progress>;
+  return (
+    <Progress
+      role="progressbar"
+      aria-label="progress bar"
+      aria-valuenow={value}
+      size={size}
+    >
+      <BarWrapper>
+        <Bar size={size} value={value} />
+      </BarWrapper>
+    </Progress>
+  );
 };
 
 export default ProgressBar;
 
-const Progress = styled.progress`
-  --borderRadius: ${(p) => (p.size === "large" ? "8px" : "4px")};
-
-  // Note, SC performs platform specific resets
-  appearance: none;
-
+const Progress = styled.div`
   height: ${(p) => heights[p.size]};
+  min-width: 370px;
+  border-radius: ${(p) => (p.size === "large" ? "8px" : "4px")};
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: ${(p) => (p.size === "large" ? "4px" : "0px")};
+`;
 
-  // TODO: Browser says "Invalid Property Value"
-  /* border-radius: var((--borderRadius)); */
+const Bar = styled.div`
+  width: ${(p) => p.value + "%"};
+  height: 100%;
+  border-radius: 4px 0 0 4px;
+  background-color: ${COLORS.primary};
 
-  &::-webkit-progress-bar {
-    border-radius: var((--borderRadius));
-    background-color: ${COLORS.transparentGray15};
-    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  }
+  // Nice :)
+  transition: width 500ms ease;
+`;
 
-  &::-webkit-progress-value {
-    border-top-left-radius: var(--borderRadius);
-    border-bottom-left-radius: var(--borderRadius);
-
-    // Nice :)
-    transition: width 500ms ease;
-
-    //The inner bar should be rounded “as-needed”
-    border-top-right-radius: ${(p) =>
-      p.value === 100 ? "var(--borderRadius)" : "0px"};
-    border-bottom-right-radius: ${(p) =>
-      p.value === 100 ? "var(--borderRadius)" : "0px"};
-    background-color: ${COLORS.primary};
-
-    // TODO Does not work
-    /* padding: ${(p) => (p.size === "large" ? "4px" : "0px")}; */
-  }
+// Exists to round inner bar edges via clipping on all sizes
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  height: 100%;
+  /* Clip inner bar to curved edge */
+  overflow: hidden;
 `;
